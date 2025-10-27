7925afec7b242dae7ac555760b76f66b4386bd0c
A-Simonsen <10632082@ucn.dk>
2025-10-27 10:03:55 +0100

    Add initial data access layer and database schema
    Created a new .NET 8.0 solution with a DataAccess project and a basic DataAccess class. Added a SQL script to create Author and BlogPost tables for the BlogSharp database.

---
+++ DataAccessLayer.sol/Create_Database.sql
@@ -0,0 +1,24 @@
+﻿USE BlogSharp;
+GO
+
+-- Authors
+CREATE TABLE dbo.Author (
+  Id           int IDENTITY(1,1) PRIMARY KEY,
+  Email        nvarchar(100) NOT NULL,
+  PasswordHash nvarchar(max) NOT NULL,
+  BlogTitle    nvarchar(50) NOT NULL,
+  CONSTRAINT UQ_Author_Email UNIQUE (Email)
+);
+GO
+
+-- Blog posts
+CREATE TABLE dbo.BlogPost (
+  Id           int IDENTITY(1,1) PRIMARY KEY,
+  Title        nvarchar(50) NOT NULL,
+  TextBody     nvarchar(max) NOT NULL,
+  CreationDate datetime2(7) NOT NULL,
+  FK_Author_Id int NOT NULL,
+  CONSTRAINT FK_BlogPost_Author FOREIGN KEY (FK_Author_Id)
+    REFERENCES dbo.Author(Id)
+);
+GO
