449d1ef2a094bd8f3a886fda49bdf08bba03b52c
A-Simonsen <10632082@ucn.dk>
2025-10-27 12:05:29 +0100

    Add Author model and DAO interface, remove DataAccess class
    Introduced the Author model and IAuthorDao interface to define data access operations for authors. Removed the empty DataAccess class. Updated the solution file to include new files and a SQL Scripts folder.

---
+++ DataAccessLayer.sol/DataAccess/IAuthorDao.cs
@@ -0,0 +1,15 @@
+﻿using DataAccess.Model;
+
+namespace DataAccess
+{
+    public interface IAuthorDao
+
+    {
+        Author? Get(int Id);
+        IEnumerable<Author> GetAll();
+        bool Delete(int Id);
+        bool Update(Author author);
+        int Create(Author author);
+
+    }
+}
